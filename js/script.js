693b9c3ab422cec6e064d3b93fa72d5f87b7dceb
makebacappiello <makebacappiello@gmail.com>
2026-09-22 09:38:07 -0400

    added event listener and testing color and design vars

--- js/script.js
+++ js/script.js
@@ -1,1 +1,25 @@
 console.log("Test");
+//after finding the name input focus on this so curser appears in the fields
+document.querySelector("#name").focus();
+
+const jobRole = document.querySelector("#title");
+const otherJobRole = document.querySelector("#other-job-role");
+console.log(jobRole);
+console.log(otherJobRole);
+
+const design = document.querySelector("#design");
+const color = document.querySelector("#color");
+console.log(color);
+console.log(design);
+console.log("JavaScript is connected!");
+//hiding the variable by default
+otherJobRole.hidden = true;
+
+//adding an event listener on change if the other option is selected from jobrole then show the other field if not stay hidden
+jobRole.addEventListener("change", (event) => {
+  if (event.target.value === "other") {
+    otherJobRole.hidden = false;
+  } else {
+    otherJobRole.hidden = true;
+  }
+});
